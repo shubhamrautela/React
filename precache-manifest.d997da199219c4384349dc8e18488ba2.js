fe3b5c2e77bec069052d321c2de494ebbb6fba51
shubhamrautela <rautelashubham@gmail.com>
2020-07-23 03:51:33 +0530

    Updates

---
+++ precache-manifest.d997da199219c4384349dc8e18488ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb43706f70a9ea22e5dcd9a179d79f60",
+    "url": "/react-based-site/index.html"
+  },
+  {
+    "revision": "6fdb8c004712f387c94e",
+    "url": "/react-based-site/static/css/main.bfa5da0c.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/react-based-site/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-based-site/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fdb8c004712f387c94e",
+    "url": "/react-based-site/static/js/main.7b340077.chunk.js"
+  },
+  {
+    "revision": "2096090fea2861bac52d",
+    "url": "/react-based-site/static/js/runtime-main.23390aa2.js"
+  }
+]);
